a9146c355268e597b9faff030847802c54f37087
Simon Balean <simonbalean@gmail.com>
2018-11-14 10:56:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms.each(attendees).each do |room_assignment|
+  assign_rooms(attendees).each do |room_assignment|
     puts room_assignment
   end
 end
